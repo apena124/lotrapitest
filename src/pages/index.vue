09ac17d8f40094ae2ff92c01ddc240a185b78380
apena124 <73714064+apena124@users.noreply.github.com>
2022-03-07 16:40:07 -0600

    the one upload

---
+++ src/pages/index.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { useAPI } from "../composables/useAPI";
+const { movies, movie, getMovies, quotes } = useAPI();
+movie.value = "";
+quotes.value = "";
+</script>
+
+<template>
+  <div class="mt-16">
+    <ul class="grid grid-cols-3 gap-12">
+      <router-link
+        class="p-4 bg-green-500 rounded-lg shadow-2xl cursor-pointer hover:scale-105"
+        v-for="movie in movies"
+        :key="movie._id"
+        :to="`/movie/${movie._id}`"
+      >
+        <div>
+          <h3 class="text-xl font-semibold tracking-tight">{{ movie.name }}</h3>
+          <p>Runtime: {{ movie.runtimeInMinutes }} minutes</p>
+          <p>Budget in Millions: ${{ movie.budgetInMillions }}</p>
+          <p>Academic Awards: {{ movie.academyAwardWins }}</p>
+          <p>Rotten Tomatoes: {{ movie.rottenTomatoesScore }}</p>
+        </div>
+      </router-link>
+    </ul>
+  </div>
+</template>
---
+++ src/pages/movie.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { useRoute } from "vue-router";
+import { useAPI } from "../composables/useAPI";
+const route = useRoute();
+const { id } = route.params;
+const { movie, getMovie, quotes, getQuotes } = useAPI();
+getMovie(id);
+getQuotes(id);
+const who = async (id) => {
+  const character = await getCharacter(id);
+  console.log(character.name);
+};
+</script>
+
+<template>
+  <div
+    v-if="movie"
+    class="max-w-md py-8 mx-auto mt-16 text-center bg-white rounded-lg"
+  >
+    <h3 class="text-xl font-semibold tracking-tight">{{ movie.name }}</h3>
+    <p>Runtime: {{ movie.runtimeInMinutes }} minutes</p>
+    <p>Budget in Millions: ${{ movie.budgetInMillions }}</p>
+    <p>Academic Awards: {{ movie.academyAwardWins }}</p>
+    <p>Rotten Tomatoes: {{ movie.rottenTomatoesScore }}</p>
+  </div>
+  <div v-else>Loading...</div>
+  <div class="mt-8">
+    <p
+      class="px-4 py-6 my-4 text-xl italic text-center bg-white rounded-lg"
+      v-for="quote in quotes"
+      :key="quote._id"
+    >
+      {{ quote.dialog }}
+    </p>
+  </div>
+</template>
